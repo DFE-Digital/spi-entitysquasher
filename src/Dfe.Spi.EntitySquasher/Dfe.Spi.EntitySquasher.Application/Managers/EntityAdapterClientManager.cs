673f2ba8bd78a33998c5d176ab1d79d693b69e4a
Matt Middleton <matt.middleton@mtcs.org.uk>
2019-12-13 11:45:45 +0000

    Implementing the manager, prior to testing.

--- src/Dfe.Spi.EntitySquasher/Dfe.Spi.EntitySquasher.Application/Managers/EntityAdapterClientManager.cs
+++ src/Dfe.Spi.EntitySquasher/Dfe.Spi.EntitySquasher.Application/Managers/EntityAdapterClientManager.cs
@@ -1,6 +1,7 @@
 ﻿namespace Dfe.Spi.EntitySquasher.Application.Managers
 {
     using System;
+    using System.Linq;
     using System.Threading.Tasks;
     using Dfe.Spi.Common.Logging.Definitions;
     using Dfe.Spi.EntitySquasher.Application.Definitions.Caches;
@@ -8,6 +9,7 @@
     using Dfe.Spi.EntitySquasher.Application.Models;
     using Dfe.Spi.EntitySquasher.Domain.Definitions;
     using Dfe.Spi.EntitySquasher.Domain.Definitions.Factories;
+    using Dfe.Spi.EntitySquasher.Domain.Models.Acdf;
 
     /// <summary>
     /// Implements <see cref="IEntityAdapterClientManager" />.
@@ -15,12 +17,17 @@
     public class EntityAdapterClientManager
         : Manager<EntityAdapterClientKey, IEntityAdapterClient>, IEntityAdapterClientManager
     {
+        private readonly IAlgorithmConfigurationDeclarationFileManager algorithmConfigurationDeclarationFileManager;
         private readonly IEntityAdapterClientFactory entityAdapterClientFactory;
 
         /// <summary>
         /// Initialises a new instance of the
         /// <see cref="EntityAdapterClientManager" /> class.
         /// </summary>
+        /// <param name="algorithmConfigurationDeclarationFileManager">
+        /// An instance of type
+        /// <see cref="IAlgorithmConfigurationDeclarationFileManager" />.
+        /// </param>
         /// <param name="entityAdapterClientCache">
         /// An instance of type <see cref="IEntityAdapterClientCache" />.
         /// </param>
@@ -31,6 +38,7 @@
         /// An instance of type <see cref="ILoggerWrapper" />.
         /// </param>
         public EntityAdapterClientManager(
+            IAlgorithmConfigurationDeclarationFileManager algorithmConfigurationDeclarationFileManager,
             IEntityAdapterClientCache entityAdapterClientCache,
             IEntityAdapterClientFactory entityAdapterClientFactory,
             ILoggerWrapper loggerWrapper)
@@ -38,14 +46,15 @@
                   entityAdapterClientCache,
                   loggerWrapper)
         {
+            this.algorithmConfigurationDeclarationFileManager = algorithmConfigurationDeclarationFileManager;
             this.entityAdapterClientFactory = entityAdapterClientFactory;
         }
 
         /// <inheritdoc />
-        protected override Task<IEntityAdapterClient> InitialiseCacheItem(
+        protected override async Task<IEntityAdapterClient> InitialiseCacheItem(
             EntityAdapterClientKey key)
         {
-            Task<IEntityAdapterClient> toReturn = null;
+            IEntityAdapterClient toReturn = null;
 
             if (key == null)
             {
@@ -56,12 +65,31 @@
             string algorithm = key.Algorithm;
             string name = key.Name;
 
-            // TODO:
             // 1) Pull back the ACDF from the manager and;
+            AlgorithmConfigurationDeclarationFile algorithmConfigurationDeclarationFile =
+                await this.algorithmConfigurationDeclarationFileManager.GetAsync(
+                    algorithm)
+                    .ConfigureAwait(false);
+
             // 2) Pull back the base URL of the right
             //    adapter using the name and;
-            // 2) Create the EntityAdapterClient via the
-            //    factory and return it.
+            if (algorithmConfigurationDeclarationFile != null)
+            {
+                EntityAdapter entityAdapter =
+                    algorithmConfigurationDeclarationFile.EntityAdapters
+                        .SingleOrDefault(x => x.Name == name);
+
+                // 2) Create the EntityAdapterClient via the
+                //    factory and return it.
+                if (entityAdapter != null)
+                {
+                    Uri baseUrl = entityAdapter.BaseUrl;
+
+                    toReturn = this.entityAdapterClientFactory.Create(
+                        baseUrl);
+                }
+            }
+
             return toReturn;
         }
     }
