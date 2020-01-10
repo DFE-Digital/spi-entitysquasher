aa02dedb38880f6ea6dc1efad6cea1365aa43e8d
Matt Middleton <matt.middleton@mtcs.org.uk>
2020-01-10 10:56:14 +0000

    Logging out ResultSquasher, and covering it in a positive unit test.

---
+++ src/Dfe.Spi.EntitySquasher/Dfe.Spi.EntitySquasher.Application.UnitTests/ResultSquasherTests.cs
@@ -0,0 +1,110 @@
+﻿namespace Dfe.Spi.EntitySquasher.Application.UnitTests
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Threading.Tasks;
+    using Dfe.Spi.Common.UnitTesting;
+    using Dfe.Spi.Common.UnitTesting.Infrastructure;
+    using Dfe.Spi.EntitySquasher.Application.Definitions.Managers;
+    using Dfe.Spi.EntitySquasher.Application.Models;
+    using Dfe.Spi.EntitySquasher.Application.Models.Result;
+    using Dfe.Spi.EntitySquasher.Domain.Models.Acdf;
+    using Dfe.Spi.Models;
+    using Moq;
+    using Newtonsoft.Json;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ResultSquasherTests
+    {
+        private Mock<IAlgorithmConfigurationDeclarationFileManager> mockAlgorithmConfigurationDeclarationFileManager;
+
+        private LoggerWrapper loggerWrapper;
+
+        private ResultSquasher resultSquasher;
+
+        [SetUp]
+        public void Arrange()
+        {
+            this.loggerWrapper = new LoggerWrapper();
+
+            this.mockAlgorithmConfigurationDeclarationFileManager =
+                new Mock<IAlgorithmConfigurationDeclarationFileManager>();
+
+            IAlgorithmConfigurationDeclarationFileManager algorithmConfigurationDeclarationFileManager =
+                mockAlgorithmConfigurationDeclarationFileManager.Object;
+
+            this.resultSquasher = new ResultSquasher(
+                algorithmConfigurationDeclarationFileManager,
+                this.loggerWrapper);
+        }
+
+        [Test]
+        public async Task SquashAsync_FeedEntityAsyncResultsAndExampleAcdf_NameResultIsExpected()
+        {
+            // Arrange
+            string algorithm = "some-algorithm";
+            string entityName = nameof(LearningProvider);
+
+            string expectedNameVariation = "SomeCorp Ltd";
+            string actualNameVariation = null;
+
+            GetEntityAsyncResult[] toSquash = new GetEntityAsyncResult[]
+            {
+                new GetEntityAsyncResult()
+                {
+                     AdapterRecordReference = new AdapterRecordReference()
+                     {
+                         Id = "123",
+                         Source = "someother-adapter"
+                     },
+                     ModelsBase = new LearningProvider()
+                     {
+                         Name = expectedNameVariation,
+                     },
+                },
+                new GetEntityAsyncResult()
+                {
+                    AdapterRecordReference = new AdapterRecordReference()
+                    {
+                        Id = "abc",
+                        Source = "another-example-adapter",
+                    },
+                    ModelsBase = new LearningProvider()
+                    {
+                        Name = "SomeCorp Limited",
+                    },
+                },
+            };
+
+            Assembly assembly = typeof(ResultSquasherTests).Assembly;
+
+            string algorithmConfigurationDeclarationFileStr =
+                assembly.GetSample("acdf-example.json");
+
+            AlgorithmConfigurationDeclarationFile algorithmConfigurationDeclarationFile =
+                JsonConvert.DeserializeObject<AlgorithmConfigurationDeclarationFile>(
+                    algorithmConfigurationDeclarationFileStr);
+
+            this.mockAlgorithmConfigurationDeclarationFileManager
+                .Setup(x => x.GetAsync(It.Is<string>(y => y == algorithm)))
+                .ReturnsAsync(algorithmConfigurationDeclarationFile);
+
+            Spi.Models.ModelsBase modelsBase = null;
+
+            // Act
+            modelsBase =
+                await this.resultSquasher.SquashAsync(
+                    algorithm,
+                    entityName,
+                    toSquash);
+
+            // Assert
+            actualNameVariation = modelsBase.Name;
+
+            Assert.AreEqual(expectedNameVariation, actualNameVariation);
+
+            string logOutput = this.loggerWrapper.ReturnLog();
+        }
+    }
+}
--- src/Dfe.Spi.EntitySquasher/Dfe.Spi.EntitySquasher.Application/ResultSquasher.cs
+++ src/Dfe.Spi.EntitySquasher/Dfe.Spi.EntitySquasher.Application/ResultSquasher.cs
@@ -46,14 +46,42 @@
         {
             Spi.Models.ModelsBase toReturn = null;
 
+            this.loggerWrapper.Debug(
+                $"Pulling back " +
+                $"{nameof(AlgorithmConfigurationDeclarationFile)} for " +
+                $"{nameof(algorithm)} = \"{algorithm}\"...");
+
             AlgorithmConfigurationDeclarationFile algorithmConfigurationDeclarationFile =
                 await this.algorithmConfigurationDeclarationFileManager.GetAsync(
                     algorithm)
                     .ConfigureAwait(false);
 
+            this.loggerWrapper.Info(
+                $"{nameof(algorithm)} = \"{algorithm}\" returned: " +
+                $"{algorithmConfigurationDeclarationFile}");
+
+            this.loggerWrapper.Debug(
+                $"Finding the entity configuration for \"{entityName}\"...");
+
             // Find the declared entity in the ACDF...
             Entity entity = algorithmConfigurationDeclarationFile.Entities
                 .Single(x => x.Name == entityName);
+
+            if (entity == null)
+            {
+                string message =
+                    $"Unable to squash the results together, as the " +
+                    $"specified {nameof(algorithm)}'s " +
+                    $"{nameof(AlgorithmConfigurationDeclarationFile)} does " +
+                    $"not specify configuration for the entity " +
+                    $"\"{entityName}\"! An entry MUST exist for " +
+                    $"\"{entityName}\".";
+
+                this.ThrowInvalidOperationException(message);
+            }
+
+            this.loggerWrapper.Info(
+                $"{nameof(Entity)} found for \"{entityName}\": {entity}.");
 
             // Create a new instance of the requested model.
             Type modelsBaseType = typeof(Spi.Models.ModelsBase);
@@ -62,15 +90,27 @@
                 modelsBaseType.Name,
                 entityName);
 
+            this.loggerWrapper.Debug(
+                $"{nameof(typeNamespace)} = \"{typeNamespace}\"");
+
             Assembly modelsAssembly = modelsBaseType.Assembly;
 
             toReturn = (Spi.Models.ModelsBase)modelsAssembly.CreateInstance(
                 typeNamespace);
 
+            this.loggerWrapper.Info(
+                $"Empty instance of \"{entityName}\" created with success: " +
+                $"{toReturn}.");
+
             // Now (attempt to) populate each property in turn.
             Type typeToReturn = modelsAssembly.GetType(typeNamespace);
 
             PropertyInfo[] propertiesToPopulate = typeToReturn.GetProperties();
+
+            this.loggerWrapper.Debug(
+                $"Cycling through {entityName}'s " +
+                $"{propertiesToPopulate.Length} properties...");
+
             foreach (PropertyInfo propertyToPopulate in propertiesToPopulate)
             {
                 this.PopulateProperty(
@@ -79,6 +119,10 @@
                     toSquash,
                     entity);
             }
+
+            this.loggerWrapper.Info(
+                $"Completed cycling through {entityName}'s " +
+                $"{propertiesToPopulate.Length} properties.");
 
             return toReturn;
         }
@@ -89,22 +133,66 @@
             IEnumerable<GetEntityAsyncResult> toSquash,
             Entity entity)
         {
-            string name = propertyToPopulate.Name;
-
             // Get an entity-level list of sources, if available.
             // This will either be null, or be populated.
             string[] sources = entity.Sources.ToArray();
+
+            string entityName = entity.Name;
+
+            string sourcesCsl = null;
+            if (sources != null)
+            {
+                sourcesCsl = string.Join(", ", sources);
+                this.loggerWrapper.Info(
+                    $"{nameof(Entity)} level sources specified for " +
+                    $"{entityName}: {sourcesCsl}.");
+            }
+            else
+            {
+                this.loggerWrapper.Debug(
+                    $"No {nameof(Entity)} level sources specified for " +
+                    $"{entityName}.");
+            }
+
+            string name = propertyToPopulate.Name;
+
+            this.loggerWrapper.Debug(
+                $"Pulling back {nameof(Field)} configuration for " +
+                $"\"{name}\"...");
 
             // Get the field declaration out of the entity.
             Field field = entity.Fields.SingleOrDefault(x => x.Name == name);
 
             if (field != null)
             {
+                this.loggerWrapper.Info(
+                    $"{nameof(Field)} configuration for \"{name}\": {field}.");
+
                 // If we have sources listed at a property level, then
                 // overwrite the sources with this.
                 if (field.Sources != null)
                 {
                     sources = field.Sources.ToArray();
+                    sourcesCsl = string.Join(", ", sources);
+
+                    this.loggerWrapper.Info(
+                        $"{nameof(Entity)} level sources were specified, " +
+                        $"but as were {nameof(Field)} level sources. The " +
+                        $"{nameof(Field)} level sources will be used " +
+                        $"instead: {sourcesCsl}.");
+                }
+
+                if (sources == null)
+                {
+                    string message =
+                        $"No sources are specified, either at an " +
+                        $"{nameof(Entity)} level, or a {nameof(Field)} " +
+                        $"level. Without the list of sources, the squasher " +
+                        $"cannot work how to compose the result. Sources " +
+                        $"MUST be specified at either an {nameof(Entity)} " +
+                        $"or {nameof(Field)} level (at minimum).";
+
+                    this.ThrowInvalidOperationException(message);
                 }
 
                 // sources now contains the list we should use.
@@ -114,23 +202,55 @@
 
                 GetEntityAsyncResult getEntityAsyncResult = null;
 
+                this.loggerWrapper.Debug(
+                    $"Looping through all {nameof(sources.Length)} " +
+                    $"source(s) until we have a result...");
+
                 string currentSource = null;
-                for (int i = 0; (i < sources.Length) && string.IsNullOrEmpty(value); i++)
+                for (int i = 0; (i < sources.Length) && this.IsFieldValueEmpty(field, value); i++)
                 {
                     // Get the next-most preferable source...
                     currentSource = sources[i];
 
                     // Get the corresponding GetEntityAsyncResult (if it
                     // exists)...
+                    this.loggerWrapper.Debug(
+                        $"Checking for result that came from " +
+                        $"\"{currentSource}\"...");
+
                     getEntityAsyncResult = toSquash
                         .SingleOrDefault(x => x.AdapterRecordReference.Source == currentSource);
 
                     if (getEntityAsyncResult != null)
                     {
+                        this.loggerWrapper.Info(
+                            $"Result found for \"{currentSource}\": " +
+                            $"{getEntityAsyncResult}.");
+
+                        this.loggerWrapper.Debug(
+                            $"Using reflection to get property value for " +
+                            $"\"{name}\" on {getEntityAsyncResult}...");
+
                         // We have one!
                         // Now use reflection to get the correpsonding value.
                         value = propertyToPopulate.GetValue(
                             getEntityAsyncResult.ModelsBase).ToString();
+
+                        if (this.IsFieldValueEmpty(field, value))
+                        {
+                            this.loggerWrapper.Info(
+                                $"Value for \"{name}\" on " +
+                                $"{getEntityAsyncResult} is considered " +
+                                $"empty. Looping will continue until a " +
+                                $"result is found, or we run out of sources.");
+                        }
+                        else
+                        {
+                            this.loggerWrapper.Info(
+                                $"Value for \"{name}\" is not considered " +
+                                $"empty, and will be used to populate our " +
+                                $"result model.");
+                        }
                     }
                     else
                     {
@@ -141,9 +261,17 @@
                     }
                 }
 
+                this.loggerWrapper.Debug(
+                    $"{nameof(value)} = \"{value}\". Updating property " +
+                    $"\"{name}\" on our result model using reflection...");
+
                 // value now has the value we need...
                 // So use reflection to update our squashed model...
                 propertyToPopulate.SetValue(modelsBase, value);
+
+                this.loggerWrapper.Info(
+                    $"\"{name}\" was updated to \"{value}\" using " +
+                    $"reflection.");
             }
             else
             {
@@ -151,6 +279,26 @@
                     $"No field entry found in {entity}! This field will not " +
                     $"be populated.");
             }
+        }
+
+        private bool IsFieldValueEmpty(
+            Field field,
+            string value)
+        {
+            bool toReturn = value == null;
+
+            // TODO: Introduce an option on the field configuration to consider
+            //       the field empty if it's an empty string, and not just
+            //       null.
+            //       Default to skipping on null only.
+            return toReturn;
+        }
+
+        private void ThrowInvalidOperationException(string message)
+        {
+            this.loggerWrapper.Error(message);
+
+            throw new InvalidOperationException(message);
         }
     }
 }
