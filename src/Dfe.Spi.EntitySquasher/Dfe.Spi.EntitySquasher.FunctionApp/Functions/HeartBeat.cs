488f5095132ea10f01c8dfde22abd7cb22105012
Matt Middleton <matt.middleton@mtcs.org.uk>
2020-01-29 11:12:33 +0000

    heart-beat method updated to be HeartBeat and same for swagger to fall in line with what EAPIM is expecting.

--- src/Dfe.Spi.EntitySquasher/Dfe.Spi.EntitySquasher.FunctionApp/Functions/HeartBeat.cs
+++ src/Dfe.Spi.EntitySquasher/Dfe.Spi.EntitySquasher.FunctionApp/Functions/HeartBeat.cs
@@ -1,5 +1,6 @@
 ﻿namespace Dfe.Spi.EntitySquasher.FunctionApp.Functions
 {
+    using System;
     using Dfe.Spi.Common.Logging.Definitions;
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
@@ -7,7 +8,9 @@
     using Microsoft.Azure.WebJobs.Extensions.Http;
 
     /// <summary>
-    /// Entry class for the <c>heart-beat</c> function.
+    /// Entry class for the <c>HeartBeat</c> function.
+    /// Note: EAPIM's health check is *not* looking for kebab-case.
+    ///       So, this is a one-off.
     /// </summary>
     public class HeartBeat
     {
@@ -26,7 +29,7 @@
         }
 
         /// <summary>
-        /// Entry method for the <c>heart-beat</c> function.
+        /// Entry method for the <c>HeartBeat</c> function.
         /// </summary>
         /// <param name="httpRequest">
         /// An instance of <see cref="HttpRequest" />.
@@ -34,12 +37,21 @@
         /// <returns>
         /// An instance of type <see cref="IActionResult" />.
         /// </returns>
-        [FunctionName("heart-beat")]
+        [FunctionName(nameof(HeartBeat))]
         public IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Function, "GET", Route = null)]
             HttpRequest httpRequest)
         {
             OkResult toReturn = null;
+
+            if (httpRequest == null)
+            {
+                throw new ArgumentNullException(nameof(httpRequest));
+            }
+
+            IHeaderDictionary headers = httpRequest.Headers;
+
+            this.loggerWrapper.SetContext(headers);
 
             this.loggerWrapper.Debug("Heart-beat function invoked.");
 
